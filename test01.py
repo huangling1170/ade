35d4340c4d17a68062ad22581a2e1c9c6b7d11ea
huangling1170 <1049215305@qq.com>
2022-03-10 16:30:05 +0800

    1235454846

--- test01.py
+++ test01.py
@@ -4,7 +4,7 @@ root =r"E:\AIdata\yellow_man2\TRAIN"
 img_name = os.listdir(root)
 img_path = f"{root}/{img_name}"
 print(img_path)
-# num,_= img_name.split(".")[0]
-# print(num)
+num,_= img_name.split(".")[0]
+print(num)
 
 
